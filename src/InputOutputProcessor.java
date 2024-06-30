604f521f8810dc93ab55fcc69a85b94d0c371522
test <armin.khosravani@gmail.com>
2024-06-30 23:51:04 +0430

    version 1

---
+++ src/InputOutputProcessor.java
@@ -0,0 +1,61 @@
+
+
+import java.util.Scanner;
+
+public class InputOutputProcessor {
+    public static final InputOutputProcessor instance = new InputOutputProcessor();
+    final Scanner scanner = new Scanner(System.in);
+    private InputOutputProcessor() {
+
+    }
+    boolean printer(CheckResult a) {
+        if(a == CheckResult.SUCCESSFUL) {
+            System.out.println("Successful!");
+            return true;
+        }
+        if(a == CheckResult.COST_ERROR) {
+            System.out.println("Invalid Cost!");
+            return true;
+        }
+        if(a == CheckResult.ID_ERROR) {
+            System.out.println("Invalid id!");
+            return true;
+        }
+        if(a == CheckResult.INVALID_COMMAND) {
+
+            System.out.println("Invalid Command!");
+            return true;
+        }
+        if(a == CheckResult.NOT_ENOUGH_CREDIT) {
+
+            System.out.println("Low Credit!");
+            return true;
+        }
+        if(a == CheckResult.PASSWORD_ERROR) {
+
+
+            System.out.println("Invalid Password!");
+            return true;
+        }
+        if(a == CheckResult.INVALID_PERCENT) {
+
+            System.out.println("Invalid Percent");
+            return true;
+        }
+        if(a == CheckResult.USER_NAME_ERROR) {
+
+            System.out.println("Invalid Username!");
+            return true;
+        }
+        return false;
+    }
+    public static InputOutputProcessor getInstance() {
+        return instance;
+    }
+
+    public Scanner getScanner() {
+        return this.scanner;
+    }
+}
+
+
